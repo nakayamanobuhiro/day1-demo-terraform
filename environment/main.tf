2f4e73bce8b2e4227bcc6d40f15ae91453463045
nakayamanobuhiro <nakayama.nobuhiro@classmethod.jp>
2023-04-10 18:23:31 +0900

    fix

--- environment/main.tf
+++ environment/main.tf
@@ -13,6 +13,14 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "example" {}
+# resource "aws_s3_bucket" "example1" {}
+
+# variable "bucket_name" {
+#   type = string
+# }
+
+# resource "aws_s3_bucket" "example2" {
+#   bucket = "${var.bucket_name}"
+# }
--- module/main.tf
+++ module/main.tf
@@ -13,4 +13,4 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_s3_bucket" "example" {}
+# resource "aws_sqs_queue" "example" {}
